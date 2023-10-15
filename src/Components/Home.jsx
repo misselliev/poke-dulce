5efc8d655a83568e3ef4727fde3ebb36d4458a7c
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-14 17:48:40 -0700

    updating buttonlist, gallery, spinner and home components

---
+++ src/Components/Home.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getPokeList } from "../Redux/pokeSlice"
+import ButtonList from "./ButtonList/ButtonList"
+import Gallery from "./Gallery/Gallery"
+import './Home.css'
+
+const Home = () => {
+    const dispatch = useDispatch()
+    const state = useSelector((state) => state.pokemon)
+    const {status} = state
+
+
+    useEffect(() => {
+        if(status === 'idle') dispatch(getPokeList())
+    }, [dispatch, status])
+
+    return(
+        <section>
+            <Gallery />
+            <ButtonList />
+        </section>
+    )
+}
+
+export default Home
