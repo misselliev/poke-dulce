e0da1ba9b315769243119427f9eaa11079649a08
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-16 12:51:11 -0700

    updating favorites funcionality

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pokeSlice from './pokeSlice'
+import favoritesSlice from './favoritesSlice'
 
 const store = configureStore({
     reducer: {
-        pokemon: pokeSlice.reducer
+        pokemon: pokeSlice.reducer,
+        favorites: favoritesSlice.reducer
     }
 })
 
